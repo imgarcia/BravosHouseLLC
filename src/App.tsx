656f42efc49db0142f7efad85cd0dfa586488d39
Mario Garcia <themariogarcia@gmail.com>
2024-01-13 02:44:18 -0600

    initial setup of app with a poc homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
 
-function App() {
+const App = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header>
+        <img
+          src="/bravoshousellc-logo.png"
+          className="app-logo"
+          alt="Bravos House LLC Logo"
+        />
       </header>
+      <div className="app-body">
+        <div className="box">
+          <h2>Welcome to our client portal</h2>
+          <p>
+            Are you ready to build your new home? Let's get you a quote right
+            away!
+          </p>
+          <p>Click on the button below to begin your quote today!</p>
+          <a className="link-button" href="#" rel="noopener noreferrer">
+            Begin My Quote
+          </a>
+        </div>
+      </div>
+      <footer>&copy; {currentYear} Bravos House, LLC</footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
